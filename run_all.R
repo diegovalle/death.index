b54c37cbd37a603386c0295dba8d9d31ea40d54f
Diego Valle-Jones <diegovalle@gmail.com>
2010-11-08 11:33:56 -0600

    source file for the military operations labels

--- run_all.R
+++ run_all.R
@@ -4,37 +4,37 @@ source('lib/boot.R')
 
 
 source("app/summary-stats.R")
+source("app/label-charts.R")
 
 #Stats for all of Mexico
-
-hom <- subset(def, PRESUNTO == 2)
-
+#Only presumed homicides
+hom <- subset(hom, PRESUNTO == 2)
 llcharts <- generateCharts(hom, 2008, "México")
 saveCharts(llcharts, "mexico")
-
 
 #Stats for Ciudad Juárez
 hom.juarez <- subset(hom, ENTOCU %in% c(08) &
                           MUNOCU %in% c(037))
 llcharts.j <- generateCharts(hom.juarez, 2008, "Cd. Juárez")
-
-op.mich <- as.Date("2006-12-11")
-op.chi <- as.Date("2008-03-27")
-llcharts.j$weekly <- llcharts.j$weekly +
-    geom_vline(aes(xintercept = op.chi), alpha = .7) +
-    geom_vline(aes(xintercept = op.mich), alpha = .7) +
-    geom_text(aes(x,y, label = "Joint Operation Chihuahua"),
-            data = data.frame(x = op.chi, y = 55),
-            size = 4, hjust = 1.01, vjust = 0) +
-    geom_text(aes(x,y, label = "Start of the Drug War"),
-            data = data.frame(x = op.mich, y = 55),
-            size = 4, hjust = 1.01, vjust = 0)
-
+llcharts.j <- labelJuarez(llcharts.j)
 saveCharts(llcharts.j, "juarez")
 
-#Other Stats
+
+#It might be worthwhile to include other states, counties, and
+#also presumed homicides + violent deaths of unknown intent
 #hom <- subset(hom, SEXO == 2)
 #hom <- subset(hom, ENTOCU %in% c(19, 28))
 #hom <- subset(hom, ENTOCU %in% c(9, 12, 15, 16, 17))
 #hom <- subset(hom, ENTOCU %in% c(2, 8, 10, 18, 25, 26))
 #005,016
+
+#Small Multiple of State Rates
+source("app/state-rates.R")
+#Small Multiple of Municipality Rates
+source("app/municipality-rates.R")
+#Choropleths
+source("app/choropleths-rates.R")
+#Clustering of Municipalities with similar homicide patterns
+#source("app/cluster.R")
+
+
