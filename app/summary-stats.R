408f10b79d126803ea52633c47b827596160ded3
Diego Valle-Jones <diegovalle@gmail.com>
2010-11-11 12:47:33 -0600

    moved the femicide rate chart to eof

--- app/summary-stats.R
+++ app/summary-stats.R
@@ -20,29 +20,6 @@ fem <- subset(hom, SEXO == 2 )
 llcharts <- generateCharts(fem, 2008, "México (Females)")
 saveCharts(llcharts, "femicide")
 
-#Mexico
-fmx <- ddply(fem, .(ANIODEF), nrow) / c(53219640, 53723982, 54216256) * 100000
-#US - FBI UNIFORM CRIME REPORTS
-fus <- c(3156, 3177, 3078) / c(151751781, 153102214, 154446928) * 100000
-
-fhom.rates <- data.frame(year = factor(2006:2008),
-                         US = fus,
-                         MX = fmx$V1)
-ggplot(melt(fhom.rates, id = "year"), aes(year, value, group = variable, color = variable)) +
-  geom_line() +
-  opts(title = "Female Homicide Rates in the US and Mexico (2006-2008)") +
-  ylab("homicide rate per 100,000 women") +
-  ylim(0, max(fhom.rates$MX)) +
-  scale_colour_manual("Country",
-                      values = c("blue", "darkgreen"))
-ggsave("graphs/femicide-rates-us-mx.png", dpi = 100,
-       width = 8, height = 6)
-
-babies <- subset(hom, SEXO != 0 & EDADVALOR < 800)
-ggplot(babies, aes(EDADVALOR, ..count.. ,group = SEXOtxt,
-                   fill = SEXOtxt))+
-  geom_histogram(position = "identity", binwidth = 1, alpha = .5)
-
 chartRegion(hom, c(08), c(037), 2008,
             "Juárez")
 chartRegion(hom, c(08), c(2, 4, 19), 2008,
@@ -87,7 +64,29 @@ hom.zac <- subset(hom, ENTOCU == 32)
 llcharts <- generateCharts(hom.zac, 2008, "Zacatecas (State)")
 saveCharts(llcharts, "zacatecas (state)")
 
-hom.count <- formatDaily(hom)
+#Femicide rate in Mexico
+#Mexico
+fmx <- ddply(fem, .(ANIODEF), nrow) / c(53219640, 53723982, 54216256) * 100000
+#US - FBI UNIFORM CRIME REPORTS
+fus <- c(3156, 3177, 3078) / c(151751781, 153102214, 154446928) * 100000
+
+fhom.rates <- data.frame(year = factor(2006:2008),
+                         US = fus,
+                         MX = fmx$V1)
+ggplot(melt(fhom.rates, id = "year"), aes(year, value, group = variable, color = variable)) +
+  geom_line() +
+  opts(title = "Female Homicide Rates in the US and Mexico (2006-2008)") +
+  ylab("homicide rate per 100,000 women") +
+  ylim(0, max(fhom.rates$MX)) +
+  scale_colour_manual("Country",
+                      values = c("blue", "darkgreen"))
+ggsave("graphs/femicide-rates-us-mx.png", dpi = 100,
+       width = 8, height = 6)
+
+babies <- subset(hom, SEXO != 0 & EDADVALOR < 800)
+ggplot(babies, aes(EDADVALOR, ..count.. ,group = SEXOtxt,
+                   fill = SEXOtxt))+
+  geom_histogram(position = "identity", binwidth = 1, alpha = .5)
 
 #chartRegion(hom, c(12), c(001, 021), 2008,
 #            "Lázaro Cárdenas")  
